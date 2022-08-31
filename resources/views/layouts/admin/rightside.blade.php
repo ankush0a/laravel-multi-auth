dfb2fee10922a8c5861c70de188537d645b54f21
ankushwpweb <ankush@wpweb.co.in>
2022-08-31 13:03:05 +0530

    Look

---
+++ resources/views/layouts/admin/rightside.blade.php
@@ -0,0 +1,9 @@
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-light">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Right Sidebar</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -6,14 +6,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ config('app.name', 'Laravel') }} | Log in</title>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Google Font: Source Sans Pro -->  
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css')}}">
   <!-- icheck bootstrap -->
   <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('css/adminlte.css')}}">
+  <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
   <link rel="shortcut icon" href="{{ asset('img/AdminLTELogo.png') }}" type="image/x-icon">
 </head>
 <body class="hold-transition login-page">
