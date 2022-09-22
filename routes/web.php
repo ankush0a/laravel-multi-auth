ab6b3fb86114bde92234e4af7026019c08a49f45
ankush0a <agrawalankush65@gmail.com>
2022-09-22 06:36:04 +0530

    Composer Update

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,17 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Auth::routes(['verify' => true]);
+Auth::routes();
 
 //  User Routes
-Route::middleware(['auth', 'user-access:user','verified'])->group(function () {
+Route::middleware(['auth', 'user-access:user'])->group(function () {
     Route::prefix('user')->group(function () {
         Route::get('/home', [App\Http\Controllers\User\HomeController::class, 'index'])->name('user.home');
     });
 });
 
 //  Admin Routes
-Route::middleware(['auth', 'user-access:admin','verified'])->group(function () {
+Route::middleware(['auth', 'user-access:admin'])->group(function () {
     Route::prefix('admin')->group(function () {
         Route::get('/home', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin.home');
     });
